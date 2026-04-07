f65935a9b66cb86b0790a1f31cbfb566270d8bdc
Shahd <shahad9mubarak@gmail.com>
2026-04-07 14:57:45 +0400

    Task 17: inOrder

---
+++ src/TASK17.java
@@ -0,0 +1,20 @@
+public class TASK17 {
+    static void main(String[] args) {
+        System.out.println(inOrder(1,2,4,false));
+        System.out.println(inOrder(1,2,1,false));
+        System.out.println(inOrder(1,1,2,true));
+}
+    public static boolean inOrder(int a, int b, int c, boolean bOk){
+        if (bOk==false) {
+            if (b > a && c > b) {
+                return true;
+            }
+        }
+        if (bOk==true){
+            if (c > b ){
+                return true;
+            }
+            }
+        return false;
+    }
+}
