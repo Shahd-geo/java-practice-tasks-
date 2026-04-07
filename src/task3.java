8e30cab0f5762b987b7d1507d9da1f5004db56e5
Shahd <shahad9mubarak@gmail.com>
2026-04-07 11:06:14 +0400

    Task 3: love6

---
+++ src/task3.java
@@ -0,0 +1,14 @@
+public class task3 {
+    static void main(String[] args) {
+        System.out.println(love6(6 , 4));
+        System.out.println(love6(4 , 5));
+        System.out.println(love6(1 , 5));
+
+    }
+    public  static boolean love6(int a, int b){
+        if (a ==6 || b==6 || a + b ==6||(Math.abs(a- b)  == 6)){
+            return true;
+        }
+        return false;
+    }
+}
