dcff5734e908ab6288706ed7b0a3da5419f43665
Shahd <shahad9mubarak@gmail.com>
2026-04-07 11:29:55 +0400

    Task 6: teaParty

---
+++ src/task6.java
@@ -0,0 +1,21 @@
+public class task6 {
+    static void main(String[] args) {
+        System.out.println(teaParty(6,8));
+        System.out.println(teaParty(3,8));
+
+    }
+    public  static int teaParty(int tea, int candy){
+        if (tea < 5 || candy < 5){
+            return 0;
+        }
+        if (tea>= 5 && candy >=5){
+            return 1;
+        }
+        if (tea>= 2* candy || candy>=2*tea){
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+}
