be0d128ac4b984652bd57b2e7b0773ada5383820
Shahd <shahad9mubarak@gmail.com>
2026-04-07 14:06:54 +0400

    Task 11: dateFashion

---
+++ src/task11.java
@@ -0,0 +1,17 @@
+public class task11 {
+    static void main(String[] args) {
+        System.out.println(dateFashion(5,10));
+        System.out.println(dateFashion(5,2));
+        System.out.println(dateFashion(5,5));
+
+    }
+    public   static int dateFashion(int you, int date){
+        if (you<= 2 || date<= 2){
+            return 0;
+        }
+        if (you>= 8 || date>=8){
+            return 2;
+        }
+        return 1;
+    }
+}
