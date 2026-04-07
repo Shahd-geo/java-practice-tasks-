acb2601ef3a22d8ade8bb69c4b1a43542615a026
Shahd <shahad9mubarak@gmail.com>
2026-04-07 15:23:57 +0400

    Task 19: redTicket

---
+++ src/TASK19.java
@@ -0,0 +1,23 @@
+public class TASK19 {
+    static void main(String[] args) {
+        System.out.println(redTicket(2, 2, 2));
+
+    }
+
+    public  static int redTicket(int a, int b, int c) {
+        if (a == 2 && b == 2 && c == 2) {
+            return 10;
+        }
+        if (a == b && b == c) {
+            return 5;
+        }
+        if (b != a && c != a) {
+            return 1;
+        }
+        return 0;
+    }
+
+}
+
+
+
