470d2f8143691df8ebb62b1a542de23a9d8f309f
Shahd <shahad9mubarak@gmail.com>
2026-04-07 14:13:29 +0400

    Task 12: sortaSum

---
+++ src/task12.java
@@ -0,0 +1,14 @@
+public class task12 {
+    static void main(String[] args) {
+        System.out.println(sortaSum(3,4));
+        System.out.println(sortaSum(9,4));
+
+    }
+    public static int sortaSum(int a, int b){
+        int sum = a + b;
+        if (sum >= 10 && sum<= 19){
+            return 20;
+        }
+        return sum;
+    }
+}
