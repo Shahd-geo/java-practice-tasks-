b1d9b0da3ef6eaa8f748c08f5f3af365cf2331b9
Shahd <shahad9mubarak@gmail.com>
2026-04-07 15:58:14 +0400

    Task 23: specialEleven

---
+++ src/TASK23.java
@@ -0,0 +1,13 @@
+public class TASK23 {
+    static void main(String[] args) {
+        System.out.println(specialEleven(22));
+        System.out.println(specialEleven(24));
+    }
+
+    public static boolean specialEleven(int n) {
+        if (n % 11 == 0 || n % 11 == 1) {
+            return true;
+        }
+        return false;
+    }
+}
