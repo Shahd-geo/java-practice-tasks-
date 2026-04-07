062a809174433057558eeec592873dd1d29c9168
Shahd <shahad9mubarak@gmail.com>
2026-04-07 11:11:02 +0400

    Task 4: more20

---
+++ src/task4.java
@@ -0,0 +1,15 @@
+public class task4 {
+    static void main(String[] args) {
+        System.out.println(more20(20));
+        System.out.println(more20(21));
+        System.out.println(more20(22));
+
+    }
+    public  static boolean more20(int n){
+        if ((n % 20 == 1 || n % 20 ==2)) {
+            return true;
+        }
+        return false;
+    }
+
+}
