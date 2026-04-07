95dc9d35c9881f244087d55372c7c0fb7808197a
Shahd <shahad9mubarak@gmail.com>
2026-04-07 10:45:52 +0400

    Task 1- cigarParty

---
+++ src/Task1.java
@@ -0,0 +1,16 @@
+public class Task1 {
+    static void main(String[] args) {
+        System.out.println(cigarParty(30,false));
+        System.out.println(cigarParty(50,false));
+        System.out.println(cigarParty(70,true));
+
+    }
+    public static boolean cigarParty(int cigars, boolean isWeekend){
+        if (isWeekend){
+            return cigars >= 40 ;
+        }
+        else {
+            return cigars >= 40 && cigars <=60;
+        }
+    }
+}
